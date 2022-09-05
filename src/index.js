67ba04bdd29fa77e928c30ff77d46fa0e505f5fe
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-09-04 20:56:39 -0500

    rutas

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}></Route>
+        <Route path='/tarotweb' element={<App />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
